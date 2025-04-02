0563c6e6d98a7518a71a4444c738daa005c5dbb0
hemanth2004 <hemanth.2004.elangovan@gmail.com>
2025-04-02 20:47:34 +0530

    wt

---
+++ core-library/transport/socket.h
@@ -0,0 +1,8 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+struct socket
+{
+}
+
+#endif
---
+++ test/test.h
@@ -0,0 +1,17 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#include <string.h>
+
+char* reverse(char* string) 
+{
+	int len = strlen(string);
+	for(int i = 0; i < len/2; i++)
+	{
+		char temp = string[i];
+		string[i] = string [len-1-i];
+		string[len-1-i] = temp; 
+	}
+}
+
+#endif
